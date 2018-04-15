7b890091e769364006397aaac5f5c68d81cf5fa5
Unknown <sten_elling@hotmail.com>
2018-04-15 20:39:07 +0200

    h

--- TP1/Ex_131.m
+++ TP1/Ex_131.m
@@ -1,25 +1,34 @@
 load('TorMod.mat')
-T = 0.04;
-CorD = 'z';
-w_cross = 0;
-par = [2, 30, w_cross];
+Ts = 0.04;
+%w_cross = 0;
+par = [2, 30];
 
 
-phi = conphi ('PID', T, 'z');
+phi = conphi ('PID', Ts, 'z');
 
-per{1} = conper ('GPhC', par);
-per{2} = conper ('GPhC', par);
-per{3} = conper ('GPhC', par);
+per = conper ('GPhC', par);
 
 G = {G1, G2, G3};
 
 K = condes(G, phi, per);
-NyquistConstr(K, G, per)
 
-S=feedback(1, G*K);
-T=feedback(G*K, 1);
-U=feedback(K, G);
+NyquistConstr(K, G1, per)
+NyquistConstr(K, G2, per)
+NyquistConstr(K, G3, per)
 
-step(T)
-bode(U)
-bode(S)
+S1=feedback(1, G1*K);
+S2=feedback(1, G2*K);
+S3=feedback(1, G3*K);
+
+T1=feedback(G1*K, 1);
+T2=feedback(G2*K, 1);
+T3=feedback(G3*K, 1);
+U1=feedback(K, G1);
+U2=feedback(K, G2);
+U3=feedback(K, G3);
+figure(4)
+step(T1,T2,T3)
+figure(5)
+bode(U1,U2,U3)
+figure(6)
+bode(S1,S2,S3)
--- TP1/Ex_151.m
+++ TP1/Ex_151.m
@@ -2,14 +2,31 @@ load('TorMod.mat')
 Te = 0.04;
 Gmm = stack(1,G1,G2,G3);
 [Gu,info]=ucover(Gmm,G3);
-W{2} = info.W1opt;
-z = tf('z',Te);
 s = tf('s');
-w_c = 6;
+z = tf('z',Te);
+w_c = 2;
 W{1} = w_c/s; % We choose W1 as an integrator because this will force S to be small at low frequencies.
+W{2} = info.W1opt;% ikke sikkert vi trenger tfest
+W{3} = tf(0.1);
+
 L_d = w_c/s;
-phi=conphi('PID',Te);
-hinfper=conper('Hinf',W,L_d);
-w = logspace(-1,4,500);
-opt = condesopt('gamma',[0.01 2 0.001],'lambda',[1 1 0 0],'w',w);
-K = condes(G3,phi,hinfper)
+phi = conphi('Laguerre',[Te , 0 ,7],'z',z/(z+1));
+hinfper = conper('Hinf',W,L_d);
+%w = 0.1:0.5:pi/Te;
+w=logspace(-1,4,500);
+opt = condesopt('gamma',[0.01 2 0.01],'lambda',[1 1 0 0],'w',w);
+%opt.nq=[];
+%opt.yalmip='on';
+K = condes(G3,phi,hinfper,opt)
+%%
+T3=feedback(G3*K, 1); 
+U3=feedback(K, G3); 
+
+stepinfo(T3)
+figure(1) 
+step(T3); 
+figure(2) 
+bode(U3)
+figure(3)
+step(U3)
+NyquistConstr(K, G3 , hinfper);
